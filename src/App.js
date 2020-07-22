4b8ee54d296eba8328388e7cd087aef0eac06b0c
alfa9317 <josealfredotg@gmail.com>
2020-07-22 13:17:53 -0500

    Material ui implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <div className="App">
+    <Button variant="contained" color="primary">
       Hello World
-    </div>
+    </Button>
   );
 }
 
