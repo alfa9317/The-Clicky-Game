b36bd731a33404dffa391c7ee1036c4d36b119df
alfa9317 <josealfredotg@gmail.com>
2020-07-21 20:45:35 -0500

    Updates

---
+++ precache-manifest.d661bfa7b442306444da7b152487d89f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3919238425c36b63766a0020cbd2060c",
+    "url": "/The-Clicky-Game/index.html"
+  },
+  {
+    "revision": "30731047c36e6ef74cd6",
+    "url": "/The-Clicky-Game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fcdf0fff6aed5800792c",
+    "url": "/The-Clicky-Game/static/js/2.d327f026.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/The-Clicky-Game/static/js/2.d327f026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30731047c36e6ef74cd6",
+    "url": "/The-Clicky-Game/static/js/main.7809bfc3.chunk.js"
+  },
+  {
+    "revision": "28868e66e0d2f9f46253",
+    "url": "/The-Clicky-Game/static/js/runtime-main.96b20b89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/The-Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
