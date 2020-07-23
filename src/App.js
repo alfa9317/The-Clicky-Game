012320cbfdefd46f834deb378914a1a542f2c21d
alfa9317 <josealfredotg@gmail.com>
2020-07-22 20:28:37 -0500

    Header basic implementation and first approach

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import Header from './components/ui/Header'
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <div>
+      <Header />
+      The Clicky Game
+    </div>
   );
 }
 
---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Appbar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+export default function Header(props) {
+    return(
+        <Appbar>
+            <Toolbar>
+                The Clicky Game
+            </Toolbar>
+        </Appbar>
+    )
+}
