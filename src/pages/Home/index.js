5b48db067b794c4a44b383f01c7f60e115c5a9b2
alfa9317 <josealfredotg@gmail.com>
2020-07-26 17:16:19 -0500

    game wrapper implementation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {makeStyles} from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Tada from 'react-reveal/Tada';
 
 import logo from "../../assets/rick-and-morty-logo.png"
@@ -9,13 +11,28 @@ const useStyles = makeStyles(theme => ({
         width: '20rem',
         display: 'block',
         margin: '0 auto',
-        marginTop: '50px',
+        marginTop: '60px',
+        marginBottom: '30px',
         [theme.breakpoints.down('sm')]:{
             width: '15rem'
         },
         [theme.breakpoints.down('xs')]:{
             width: '12rem'
         }
+    },
+    gameContainer:{
+        margin: '10%',
+        padding: '20%',
+        paddingTop: '0px',
+        marginTop: '0%',
+        background: theme.palette.common.black,
+        height: '70%',
+        display: 'flex',
+        flexFlow: 'row wrap',
+        padding: '20px',
+        justifyContent: 'space-around',
+        alignContent: 'flex-start',
+        overflow: 'auto'
     }
 }))
 
@@ -28,7 +45,10 @@ export default function Home(props){
         <div>
             <Tada>
                 <img alt="rick and morty logo" src={logo} className={classes.logo}/>
-            </Tada>    
+            </Tada>
+            <div className={classes.gameContainer}>
+
+            </div>    
         </div>
         
       );
