5a66b56d8496e35fc9f27f9c9adb5f8736840293
alfa9317 <josealfredotg@gmail.com>
2020-07-26 11:17:59 -0500

    Implement title logo of the Home Page using React Reveal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,34 @@
 import React from "react";
+import {makeStyles} from '@material-ui/styles';
+import Tada from 'react-reveal/Tada';
+
+import logo from "../../assets/rick-and-morty-logo.png"
+
+const useStyles = makeStyles(theme => ({
+    logo: {
+        width: '20rem',
+        display: 'block',
+        margin: '0 auto',
+        marginTop: '50px',
+        [theme.breakpoints.down('sm')]:{
+            width: '15rem'
+        },
+        [theme.breakpoints.down('xs')]:{
+            width: '12rem'
+        }
+    }
+}))
 
 export default function Home(props){
+
+    const classes = useStyles()
 
     return (
         
         <div>
-             <h1>
-                 Home Page 
-             </h1>
-            
+            <Tada>
+                <img alt="rick and morty logo" src={logo} className={classes.logo}/>
+            </Tada>    
         </div>
         
       );
