2de427aceced2c5fb94ccaa4ac2193f673556f13
alfa9317 <josealfredotg@gmail.com>
2020-07-29 14:31:02 -0500

    Add information in about page and adjust margins

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -12,20 +12,18 @@ const useStyles = makeStyles( theme =>({
     title: {
         ...theme.typography.text,
         fontSize: '4rem',
-        marginTop: '50px',
-        marginBottom: '5px',
+        marginTop: '60px',
+        marginBottom: '30px',
         color: 'white',
         [theme.breakpoints.down('md')]:{
             fontSize: '3.5rem',
-            margin: '40px'
-        },
-        [theme.breakpoints.down('sm')]:{
-            fontSize: '3rem',
-            margin: '30px'
+            marginTop: '40px',
+            marginBottom: '30px'
         },
         [theme.breakpoints.down('xs')]:{
             fontSize: '2rem',
-            margin: '20px'
+            marginTop: '40px',
+            marginBottom: '20px'
         }
     },
     image: {
@@ -37,8 +35,39 @@ const useStyles = makeStyles( theme =>({
             width: '20rem'
         }
     },
+    text: {
+        color: 'white',
+        fontFamily: 'Orbitron',
+        marginLeft: '25rem',
+        marginRight: '25rem',
+        marginTop: '40px',
+        textAlign:'center',
+        [theme.breakpoints.down('md')]:{
+            marginLeft: '15rem',
+            marginRight: '15rem'
+        },
+        [theme.breakpoints.down('sm')]:{
+            marginLeft: '10rem',
+            marginRight: '10rem'
+        },
+        [theme.breakpoints.down('xs')]:{
+            marginLeft: '5rem',
+            marginRight: '5rem'
+        }
+    },
     pad: {
-        height: '600px'
+        height: '400px'
+    },divider: {
+        color: 'orange',
+        textAlign: 'center',
+        [theme.breakpoints.down('xs')]:{
+            fontSize:'22px'
+        }
+    },
+    description: {
+        fontSize:'0.7rem',
+        color: 'white',
+        fontFamily: 'Orbitron'
     }
 }))
 
@@ -49,12 +78,34 @@ export default function About(props){
     return (
         
         <div className={classes.mainContainer}>
+             
              <Grid container direction="column" justify="center" alignItems="center" spacing={2}>
                 <Grid item className={classes.title}>
                      About the game
                 </Grid>
                 <Grid item className={classes.image}>
                     <img alt='game theme image' src={gameTheme} className={classes.image} />
+                 </Grid>
+                 <Grid item className={classes.text}>
+                    Welcome to Rick and Morty The Infinite Fracture! 
+                 </Grid>
+                 <Grid item className={classes.text}>
+                    The universe has been fractured into infinite copies of the same dimension. Only Rick can fix it, but this time he will need your help.
+                    <br/>
+                    <br/>
+                    Your role will be to choose only one copy of each character, otherwise you will have to do it all over again.
+                    <br/>
+                    <br/> 
+                    Good luck and remember that the future of the universe is in your hands (literaly).
+                 </Grid>
+                 <Grid item className={classes.text}>
+                    Wubba Lubba Dub Dub!!! 
+                 </Grid>
+             </Grid>
+             <h1 className={classes.divider}>--------------------------------</h1>
+             <Grid container direction="column" justify="center" alignItems="center">
+                <Grid item className={classes.description}>
+                    This is a memory game done with ReactJS and Material UI.  
                  </Grid>
                  <Grid item className={classes.pad}/>
              </Grid>
